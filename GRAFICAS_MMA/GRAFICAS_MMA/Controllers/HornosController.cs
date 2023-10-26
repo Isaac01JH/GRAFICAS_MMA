f27b573aa5c739a2e3074bee8afd3ab5e3b9598a
Isaac01JH <102643113+Isaac01JH@users.noreply.github.com>
2023-10-26 08:44:30 -0600

    Add files via upload

---
+++ GRAFICAS_MMA/GRAFICAS_MMA/Controllers/HornosController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GRAFICAS_MMA.Controllers
+{
+    public class HornosController : Controller
+    {
+        // GET: HornosController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: HornosController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: HornosController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: HornosController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: HornosController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: HornosController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: HornosController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: HornosController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
